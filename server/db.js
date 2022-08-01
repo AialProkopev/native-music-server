b8fa2afec948c05797d4d8c66493616ad1e55a35
Ayal <freezzforever@gmail.com>
2022-08-01 20:01:58 +0300

    commit

---
+++ server/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+module.exports = async() => {
+    try {
+        await mongoose.connect(process.env.DB)
+        console.log('Connected to database Successfully');
+    } catch (error) {
+       console.log('Could not connect to database'); 
+    }
+}
+
